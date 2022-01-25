1977a7b11f82700dd7b01f0427649bd6de1a829c
Ubuntu <ubuntu@ip-172-31-30-175.ec2.internal>
2022-01-25 22:47:36 +0000

    Gonzalo Lab 1 fixed spacing

--- labs/1/lab_1.cpp
+++ labs/1/lab_1.cpp
@@ -24,25 +24,25 @@ std::vector<int> failed_tests;
 // Program Execution Starts Here
 int main() {
   // (1) Declare a string variable named user_name
-string user_name;
+  string user_name;
   // (2) Declare a character variable named programmed_before and another named
   //     my_character. Initialize both of them to the value 'z'
-char programmed_before = 'z' , my_character = 'z';
+  char programmed_before = 'z' , my_character = 'z';
   // (3) Display a welcome message to standard output
-cout << "Welcome" << endl;
+  cout << "Welcome" << endl;
   // (4) Prompt the user for their first name
-cout << "Please eneter your first name: ";
+  cout << "Please eneter your first name: ";
   // (5) Read the name from standard input and store in the variable user_name
-cin >> user_name;
+  cin >> user_name;
   // (6) Display the message "Nice to meet you, NAME" where NAME is replaced
   //     with the value of user_name
-cout << "Nice to meet you, " << user_name << endl;
+  cout << "Nice to meet you, " << user_name << endl;
   // (7) Ask the user if they have programmed before. Make sure you inform them
   //     that you want the answer as a single character (y/n)
-cout << "Have you programmed before?(y/n): ";
+  cout << "Have you programmed before?(y/n): ";
   // (8): Read in the answer from standard input and store in the variable
   //      programmed_before
-cin >> programmed_before;
+  cin >> programmed_before;
   // For testing (DO NOT ALTER)
   UnitTest(user_name, programmed_before, my_character);
 
