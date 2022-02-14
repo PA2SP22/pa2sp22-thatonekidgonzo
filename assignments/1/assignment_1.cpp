10dbd443308013ca0f04fc8e162b5e70720be1cd
Gonzalo Garcia <ggarcia011@student.butte.edu>
2022-02-14 22:28:20 +0000

    Adding Assignment 1

--- assignments/1/assignment_1.cpp
+++ assignments/1/assignment_1.cpp
@@ -1,10 +1,54 @@
 /*
- * Name        : FILL IN
- * Author      : FILL IN
- * Description : FILL IN
+ * Name        : Gonzalo Garcia
+ * Author      : Gonzalo Garcia
+ * Description : Assignment #1
  * Sources     : FILL IN
  */
 
 #include "assignment_1.h"
 
 // Write Function Definitions Here (What goes below main)
+  bool CheckAlphabetic(const string &to_check) {
+    if (to_check.length() > 0) {
+      for (int i = 0; i < to_check.length(); i++) {
+        }
+      if (isalpha(to_check.at(i)) == 0) {
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+      
+   } 
+
+
+  bool EncryptString(string string_to_encrypt, int shift_number) {
+    
+    return false;
+  }
+  
+  
+  bool DecryptString(string string_to_decrypt, int shifted_number) {
+    
+    return false;
+  }
+  
+  
+  double ComputeAverage(double[], unsigned int size_of_array) {
+    
+    return 1.0;
+  }
+  
+  
+  double FindMinValue(double[], unsigned int size_of_array) {
+    
+    return 1.0;
+  }
+  
+  
+  double FindMaxValue(double[], unsigned int size_of_array) {
+    
+    return 1.0;
+  }
--- assignments/1/assignment_1.h
+++ assignments/1/assignment_1.h
@@ -1,6 +1,6 @@
 /*
- * Name        : FILL IN
- * Author      : FILL IN
+ * Name        : Gonzalo Garcia
+ * Author      : Gonzalo Garcia
  * Description : FILL IN
  * Sources     : FILL IN
  */
@@ -11,6 +11,16 @@
 // Add any includes and using statements Here
 
 // Declare Function Prototypes Here (What goes above main)
+bool CheckAlphabetic(const string &to_check);
 
+bool EncryptString(string string_to_encrypt, int shift_number);
+
+bool DecryptString(string string_to_decrypt, int shifted_number);
+
+double ComputeAverage(double[], unsigned int size_of_array);
+
+double FindMinValue(double[], unsigned int size_of_array);
+
+double FindMaxValue(double[], unsigned int size_of_array);
 
 #endif /* ASSIGNMENT_1_H */
