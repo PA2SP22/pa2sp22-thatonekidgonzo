a2fc174b45ced66c6e884e02d850f3991f22ad29
Gonzalo Garcia <ggarcia011@student.butte.edu>
2022-02-18 07:29:40 +0000

    Adding Lab #7

--- labs/7/lab_7.cpp
+++ labs/7/lab_7.cpp
@@ -1,6 +1,6 @@
 /*
  * Name        : lab_7.cpp
- * Author      : FILL IN
+ * Author      : Gonzalo Garcia
  * Description : Working with File I/O
  */
 #include <fstream>
@@ -57,7 +57,34 @@ int main() {
 }
 
 // CODE HERE -- FUNCTION DEFINITION
+bool ProcessFile(string filename) {
+  string line;
+  ifstream fin(filename);
+  if (fin.fail()) {
+    std::cerr << "Error opening one of the files.\n";
+    exit(1);
+    return false;
+  }
 
+  while (!fin.eof()) {
+    getline(fin, line);
+    if (line == "10") {
+      OnTen();
+    } else if (line == "20") {
+      OnTwenty();
+    } else if (line == "30") {
+      OnThirty();
+    } else if (line == "40") {
+      OnForty();
+    } else if (line == "50") {
+      OnFifty();
+    } else {
+      OnError();
+    }
+  }
+  fin.close();
+  return false;
+}
 // For testing (DO NOT ALTER)
 void UnitTest() {
   cout << string(40, '-') << endl;
