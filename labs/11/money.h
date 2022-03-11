a56874306e7ab4c43dcc182947399317aba614cb
Gonzalo Garcia <ggarcia011@student.butte.edu>
2022-03-11 03:05:55 +0000

    Adding Lab #11

--- labs/11/money.h
+++ labs/11/money.h
@@ -1,10 +1,13 @@
 /*
  * Name        : money.h
- * Author      : FILL IN
+ * Author      : Gonzalo Garcia
  * Description : Class Header File
  */
 
 // ADD HEADER GUARD HERE
+#ifndef GARCIA_MY_CLASS_
+#define GARCIA_MY_CLASS_
+
 
 #include <string>
 #include <iostream>
@@ -113,3 +116,4 @@ class Money {
 };
 
 // REST OF HEADER GUARD GOES HERE
+#endif
